e4ded55caf91d53e2b9c157a99af8431a067bb80
Rafael Moreno <rafaelmorenosax@gmail.com>
2024-09-23 20:21:59 -0300

    lista cliente json

--- AulaAngular/src/app/cliente/lista-cliente/lista-cliente.component.ts
+++ AulaAngular/src/app/cliente/lista-cliente/lista-cliente.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Cliente } from 'src/app/models/Cliente';
 
 @Component({
   selector: 'app-lista-cliente',
   templateUrl: './lista-cliente.component.html',
-  styleUrls: ['./lista-cliente.component.css']
+  styleUrls: ['./lista-cliente.component.css'],
 })
 export class ListaClienteComponent {
-
+  cliente: Cliente[] = [
+    { id: 1, nome: 'João', celular: '999999999', email: 'teste@teste' },
+    { id: 2, nome: 'José', celular: '999999999', email: 'teste@teste' },
+    { id: 3, nome: 'Maria', celular: '999999999', email: 'teste@teste' },
+  ];
 }
