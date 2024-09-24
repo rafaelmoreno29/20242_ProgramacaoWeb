458421eb94cad779efc304b07dc0f9a5129824fe
Rafael Moreno <rafaelmorenosax@gmail.com>
2024-09-23 21:47:36 -0300

    rota aninhada e parametro

--- AulaAngular/src/app/cliente/editar-cliente/editar-cliente.component.ts
+++ AulaAngular/src/app/cliente/editar-cliente/editar-cliente.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-editar-cliente',
   templateUrl: './editar-cliente.component.html',
-  styleUrls: ['./editar-cliente.component.css']
+  styleUrls: ['./editar-cliente.component.css'],
 })
-export class EditarClienteComponent {
-
+export class EditarClienteComponent implements OnInit {
+  id: number = 0;
+  constructor(public route: ActivatedRoute) {}
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'] ?? 0;
+  }
 }
