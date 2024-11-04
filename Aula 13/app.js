ce15d46511d23544ac1575c96acdea4d7e3d6a2f
Rafael Moreno <rafaelmorenosax@gmail.com>
2024-11-04 20:29:44 -0300

    aula 14

--- Aula 13/app.js
+++ Aula 13/app.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+
 const usuarioRouter = require('./routes/usuario');
 const lista1Router = require('./routes/lista1');
 
@@ -8,4 +14,13 @@ app.use(express.json());
 app.use('/usuario', usuarioRouter);
 app.use('/lista1', lista1Router);
 
-app.listen(3000, () => console.log('Servidor rodando na porta: 3000'));
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.oy8p7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+    .then(() => {
+        app.listen(3000, () => {
+            console.log('Conectado ao mongoDB');
+            console.log('Servidor iniciado na porta 3000');
+        })
+    })
+    .catch((err) => {
+        console.log(err);
+    });
--- Aula 13/controllers/usuarioController.js
+++ Aula 13/controllers/usuarioController.js
@@ -1,9 +1,16 @@
+const usuarioModel = require('../models/usuario');
+
 // Lógica para obter todos os usuários
 exports.obterTodos = (req, res) => {
     res.json({ "mensagem": "Obter todos os usuários" });
 };
 
 // Lógica para criar um novo usuário
-exports.inserir = (req, res) => {
-    res.status(201).json({ "mensagem": "Inserir um novo usuário" });
+exports.inserir = async (req, res) => {
+    try {
+        await usuarioModel.create(req.body);
+        res.status(201).json(req.body);
+    } catch (error) {
+        res.status(400).json({ error: error });
+    }
 };
