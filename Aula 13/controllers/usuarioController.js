db9c8b516b4c06e76dec58dc0c3639a0f89efdcf
Rafael Moreno <rafaelmorenosax@gmail.com>
2024-11-04 21:25:51 -0300

    aluno model

--- Aula 13/controllers/usuarioController.js
+++ Aula 13/controllers/usuarioController.js
@@ -6,7 +6,7 @@ exports.obterTodos = async (req, res) => {
         const usuarios = await usuarioModel.find();
         res.status(200).json(usuarios);
     } catch (error) {
-        res.status(500).json({ error: error });
+        res.status(400).json({ error: error });
     }
 };
 
