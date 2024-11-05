c70fc024074abfd09c67744704af7549d37be7eb
Rafael Moreno <rafaelmorenosax@gmail.com>
2024-11-04 21:34:27 -0300

    aluno appjs

--- Aula 13/app.js
+++ Aula 13/app.js
@@ -7,12 +7,15 @@ const DB_PASSWORD = process.env.DB_PASSWORD
 
 const usuarioRouter = require('./routes/usuario');
 const lista1Router = require('./routes/lista1');
+const alunoRouter = require('./routes/aluno');
+
 
 const app = express();
 app.use(express.json());
 
 app.use('/usuario', usuarioRouter);
 app.use('/lista1', lista1Router);
+app.use('/aluno', alunoRouter);
 
 mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.oy8p7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
     .then(() => {
